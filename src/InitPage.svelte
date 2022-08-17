95cc08c5f2b0a1307182bd5f2737f69c3695b7b2
spark380@naver.com <spark380@naver.com>
2022-08-17 17:05:57 +0900

    clean Readme 승인 바랍니다.

---
+++ src/InitPage.svelte
@@ -0,0 +1,55 @@
+<script>
+    
+let iInvaildCnt = 0;
+
+function onClickedLogin(){
+    iInvaildCnt = iInvaildCnt + 1;
+}
+</script>
+
+<div id="dv_Main">    
+    <div id="dv_Sub">    
+        <label class="lb"> 아이디:</label>
+        <input class="edt" type="text"/>            
+        <br/>
+        <label class="lb">비밀번호:</label>
+        <input class="edt" type="password"/>
+        <br/>
+        <label class="lb"></label>
+        <button id="btn_Login"type="button" on:click={onClickedLogin}>로그인</button>
+        <br/>        
+        <label class="lb"></label>
+        <label class="lb">{iInvaildCnt}</label>
+    </div>
+</div>
+<style>
+    #dv_Main{
+        margin-top: 50px;
+        /*ackground-color: antiquewhite;*/
+    }
+    #dv_Sub{
+        margin-top: 50px;
+        /*background-color: green;*/
+        width: 400px;
+        display: inline-block;        
+    }
+    .lb{
+        margin-top: 10px;        
+        margin-right: 10px;        
+        width: 100px;
+        display: inline-block;        
+        justify-items: center;
+        text-align: center;
+        /*background-color: green;*/
+    }
+    .edt{
+        width: 150px;
+        margin-top:   20px;        
+        margin-right: 10px;        
+    }
+    #btn_Login{
+        width: 160px;
+        background-color: mediumspringgreen;
+        margin-top: 20px;
+    }
+</style>
