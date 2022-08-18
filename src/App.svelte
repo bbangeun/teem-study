1003440ebc4530e7639d868b0eec46489f0d11e0
spark380@naver.com <spark380@naver.com>
2022-08-18 17:26:34 +0900

    Layout테스트 페이지 추가

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
     import Counter from './lib/Counter.svelte';  
     import InitPage from "./InitPage.svelte";
     import Mosaic from './Mosaic.svelte';  
+	import DynamicLayout from './DynamicLayout.svelte';
     import {Router, Link, Route} from "svelte-routing";
 
     export let url ="";	
@@ -12,10 +13,12 @@
 	<nav>
 		<Link to="/">InitPage</Link>		
 		<Link to="Mosaic">Mosaic</Link>		
+		<Link to="DynamicLayout">Layout</Link>		
 	</nav>
 	<div>
 		<Route path="/"><InitPage/></Route>
 		<Route path="Mosaic" component={Mosaic}></Route>
+		<Route path="DynamicLayout" component={DynamicLayout}></Route>
 	</div>
 </Router>
 
